ae36f9a4d6693c612afb6d787ec78317ac211972
claudioed <claudioed.oliveira@gmail.com>
2023-11-25 15:05:15 -0300

    add more logs

--- src/main/java/com/paymentic/adapter/kafka/OrderStarterProcessor.java
+++ src/main/java/com/paymentic/adapter/kafka/OrderStarterProcessor.java
@@ -6,15 +6,20 @@ import jakarta.enterprise.event.Observes;
 import jakarta.enterprise.event.TransactionPhase;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.jboss.logging.Logger;
 
 @ApplicationScoped
 public class OrderStarterProcessor {
+  private static final Logger LOGGER = Logger.getLogger(OrderStarterProcessor.class);
   private final Emitter<PaymentOrderStartedEvent> paymentOrderStartedEventEmitter;
+
   public OrderStarterProcessor(@Channel("payment-order-started") Emitter<PaymentOrderStartedEvent> paymentOrderStartedEventEmitter) {
     this.paymentOrderStartedEventEmitter = paymentOrderStartedEventEmitter;
   }
   public void notify(@Observes(during = TransactionPhase.AFTER_SUCCESS) PaymentOrderStartedEvent event){
+    LOGGER.info("Emitting payment order started...");
     this.paymentOrderStartedEventEmitter.send(event);
+    LOGGER.info("Payment Order started emitted!!!");
   }
 
 }
--- src/main/java/com/paymentic/adapter/kafka/TransactionResultProcessor.java
+++ src/main/java/com/paymentic/adapter/kafka/TransactionResultProcessor.java
@@ -7,9 +7,11 @@ import jakarta.enterprise.event.Observes;
 import jakarta.enterprise.event.TransactionPhase;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.jboss.logging.Logger;
 
 @ApplicationScoped
 public class TransactionResultProcessor {
+  private static final Logger LOGGER = Logger.getLogger(TransactionResultProcessor.class);
   private final Emitter<TransactionProcessedEvent> transactionFailedEmitter;
   private final Emitter<TransactionProcessedEvent> transactionApprovedEmitter;
 
@@ -19,11 +21,13 @@ public class TransactionResultProcessor {
     this.transactionApprovedEmitter = transactionApprovedEmitter;
   }
   public void notify(@Observes(during = TransactionPhase.AFTER_SUCCESS  ) TransactionProcessedEvent transactionProcessedEvent){
+    LOGGER.info("Emitting payment order processed...");
     if (TransactionStatus.APPROVED.equals(transactionProcessedEvent.status())){
       this.transactionApprovedEmitter.send(transactionProcessedEvent);
     }else {
       this.transactionFailedEmitter.send(transactionProcessedEvent);
     }
+    LOGGER.info("Payment order processed emitted");
   }
 
 }
